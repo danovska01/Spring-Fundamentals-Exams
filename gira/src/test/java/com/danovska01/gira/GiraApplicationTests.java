686c38a81f97adc03d3d25a9bc22cea2c6b0209b
danovska01 <kristina.danovska@gmail.com>
2023-09-12 10:32:21 +0300

    initiallizing the project, pom, yml, resources ready

---
+++ gira/src/test/java/com/danovska01/gira/GiraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danovska01.gira;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GiraApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
